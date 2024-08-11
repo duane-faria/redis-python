710dcc51837ddfcb862057c76bd21378c1848141
Duane Faria <duane_gabriel@hotmail.com>
2024-08-10 22:39:29 -0300

    answer to multiple pings

--- app/main.py
+++ app/main.py
@@ -8,15 +8,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, address = server_socket.accept()
     # wait for client
     print(address)
     with conn:
-        conn.recv(1024)
-        conn.send(PONG.encode())
+        while 1 == 1:
+            command = conn.recv(1024)
+
+            conn.send(PONG.encode())
+
 
 if __name__ == "__main__":
     main()
