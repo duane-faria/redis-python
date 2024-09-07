aa6cc1a4e2cba37b2a5ed7f5096945a6e92a904f
Duane Faria <duane_gabriel@hotmail.com>
2024-09-07 19:31:04 -0300

    feat: add to the decode resp class the possibility to decode arrays of arrays

--- app/resp_handlers.py
+++ app/resp_handlers.py
@@ -1,3 +1,5 @@
+import re
+
 ARRAY_IDENTIFIER: str = '*'
 SIMPLE_STRING_IDENTIFIER: str = '+'
 BULK_STRING_IDENTIFIER: str = '$'
@@ -6,17 +8,44 @@ ENCODING_DIVIDER="\r\n"
 class RESPParser:
     def __init__(self, data: bytes):
         self.data = data.decode('utf-8')
-    
+
+    def get_all_array_positions(self):
+        asterisks = [m.start() for m in re.finditer(r'\*', self.data)]
+        return asterisks
+
     def parse(self):
         data_identifier = self.data[0]
-
+        response = ['', '']
         if data_identifier == ARRAY_IDENTIFIER:
-            return self._parse_array()
+            has_multiple_arrays = len(self.get_all_array_positions()) > 1
+            if not has_multiple_arrays:
+                response = self._parse_array()
+            else:
+                print('caiu no else do array')
+                exclude_list = [ARRAY_IDENTIFIER, BULK_STRING_IDENTIFIER]
+                elements: list[str] = self.data.split('\r\n')
+                message_elements = []
+                sub_list = []
+                counter = 0
+                for element in elements:
+                    if not len(element) > 0: # if empty skip
+                        continue
 
+                    element_has_identifier = element[0] in exclude_list
+                    if element[0] == ARRAY_IDENTIFIER and counter > 1:
+                        message_elements.append(sub_list)
+                        sub_list = []
+                    if not element_has_identifier:
+                        sub_list.append(element)
+                    counter += 1
+
+                message_elements.append(sub_list)
+                response = message_elements
+                print(response)
         if data_identifier == SIMPLE_STRING_IDENTIFIER:
-           return self._parse_simple_string()
+           response = self._parse_simple_string()
 
-        return ['', '']
+        return response
 
     def _parse_array(self):
         broken_data = self.data.split('\r\n')
@@ -29,8 +58,8 @@ class RESPParser:
 
     def _filter_values(self, value: str, index: int) -> bool:
         return index > 0 and len(value) > 0 and value[0] != '$'
-    
-    
+
+
 class RESPEncoder:
     def __init__(self):
         pass
@@ -70,7 +99,7 @@ class RESPEncoder:
                 items.append(f'{BULK_STRING_IDENTIFIER}{len(item)}')
                 items.append(item)
 
-            return f"{ARRAY_IDENTIFIER}{array_length}{ENCODING_DIVIDER}{ENCODING_DIVIDER.join(items)}{ENCODING_DIVIDER}".encode("utf-8")      
-        
+            return f"{ARRAY_IDENTIFIER}{array_length}{ENCODING_DIVIDER}{ENCODING_DIVIDER.join(items)}{ENCODING_DIVIDER}".encode("utf-8")
+
 
 #  python3 -m app.resp_handlers.py
